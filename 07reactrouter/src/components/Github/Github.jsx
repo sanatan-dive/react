b291afb8ce590dd3ab8efbb8f0779c738ce61bdc
sanatan-dive <sanatansharma352@gmail.com>
2024-10-09 20:01:45 +0530

    adding 7th project

--- 07reactrouter/src/components/Github/Github.jsx
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -39,3 +39,5 @@ export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/sanatan-dive')
     return response.json()
 }
+
+//preprocessing preloading
